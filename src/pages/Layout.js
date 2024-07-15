8636a50bccc3bf972031ccc2d569399047192773
Dilmurod <nookh@ya.ru>
2024-07-16 03:11:36 +0500

    aaa

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,6 +4,8 @@ import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPeopleArrows, faHome, faTasks } from '@fortawesome/free-solid-svg-icons';
 import './Style.css';
+import {useTelegram} from "./hooks/useTelegram";
+
 const tabs = [{
     route: "/",
     icon: faHome,
@@ -17,8 +19,17 @@ const tabs = [{
     icon: faTasks,
     label: "Tasks"
   }]
+
+const {user, tg} = useTelegram();
+
+  useEffect(() => {
+      tg.ready();
+  }, [])
+
 const Layout = () => {
   return (
+    <>
+    <span className={'username'}>{user?.username} {user?.photo_url} {user?.first_name}</span>
     <div className="col-lg-4">
     <nav className="navbsar navbar-light navbar-expand rounded-pill mb-3 ms-3 me-3 fixed-bottom d-md-none d-lg-none d-xl-none shadow" role="navigation">
       <Nav className="w-100">
@@ -40,6 +51,7 @@ const Layout = () => {
     </nav>
     <Outlet />
     </div>
+    </>
   )
 };
 
