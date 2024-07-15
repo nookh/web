b985f73579795a342b67864fba1b9952be24784b
Dilmurod <nookh@ya.ru>
2024-07-15 14:24:23 +0500

    star

--- src/index.js
+++ src/index.js
@@ -16,29 +16,12 @@ export default function App() {
         frameRate={30}
         starColor={"rainbow"}
       />
-
-      <StarSky
-        debugFPS={false}
-        // isPageBackground={false}
-        starColor={[0, 0, 255]}
-        backgroundColor={[0, 0, 10]}
-        style={{
-          width: '300px',
-          height: '300px',
-          margin: '0 auto',
-          marginTop: '100px',
-          border: '1px solid grey',
-          borderRadius: '10px',
-          padding: '5px'
-        }}
-      >
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
         </Route>
       </Routes>
-      </StarSky>
     </BrowserRouter>
   );
 }
