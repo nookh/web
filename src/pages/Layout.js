ff8668ec23754de78dac6aec5ad210df62083139
Dilmurod <nookh@ya.ru>
2024-07-15 09:12:51 +0500

    styling layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,23 +1,34 @@
 import { Outlet, Link } from "react-router-dom";
+import { Nav, NavItem} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faHome, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <div>
+    <nav className="navbar fixed-bottom navbar-light d-block d-lg-none bottom-tab-nav" role="navigation">
+      <Nav className="w-100">
+        <div className=" d-flex flex-row justify-content-around w-100">
+          {
+            tabs.map((tab, index) =>(
+              <NavItem key={`tab-${index}`}>
+                <NavLink to={tab.route} className="nav-link bottom-nav-link" activeClassName="active">
+                  <div className="row d-flex flex-column justify-content-center align-items-center">
+                    <FontAwesomeIcon size="lg" icon={tab.icon}/>
+                    <div className="bottom-tab-label">{tab.label}</div>
+                  </div>
+                </NavLink>
+              </NavItem>
+            ))
+          }
+        </div>
+      </Nav>
+    </nav>
+    </div>
 
-      <Outlet />
+    <Outlet />
     </>
   )
 };
