72c2c06441ca05de1ca487c576690ce4640ec114
Dilmurod <nookh@ya.ru>
2024-07-25 19:49:12 +0500

    aaa

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export default function App() {
     <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/blogs/:id" element={<Blogs />} />
+          <Route exact path="/blogs/:id" element={<Blogs />} />
         </Route>
       </Routes>
   </BrowserRouter>
