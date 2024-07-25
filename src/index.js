71bb0a672ce2a9bce95a699109b63281e41e63e8
Dilmurod <nookh@ya.ru>
2024-07-25 18:18:02 +0500

    aa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import 'bootstrap/dist/css/bootstrap.css';
-import Background from "./file";
 
 export default function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/blog" component={Blogs} />
