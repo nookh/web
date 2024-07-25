81b8abdf64ef145ea58dbed4713049cc8f841026
Dilmurod <nookh@ya.ru>
2024-07-25 13:32:00 +0500

    a

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -18,7 +18,6 @@ export function useTelegram() {
         onClose,
         onToggleButton,
         tg,
-        data: tg.initData,
         user: tg.initDataUnsafe?.user,
         queryId: tg.initDataUnsafe?.query_id,
     }
