c74f1fd4b57cbee0856f9ef794d8af972be49cae
Dilmurod <nookh@ya.ru>
2024-08-11 20:45:43 +0500

    bottom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import Game from "./pages/Game.tsx";
 import ProfileBar from "./pages/ProfileBar.tsx";
+import BottomNav from "./pages/BottomNav.tsx";
 //import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className="xl:w-[30%] lg:w-[50%] md:w-[70%] w-full h-screen bg-gradient-to-t from-[#0b0c0e] to-[#00446d] flex flex-col justify-between my-0 mx-auto px-5 py-3">
       <ProfileBar/>
       <Game/>
+      <BottomNav/>
     </div>
   );
 }
