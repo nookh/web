baddb5a57431b83bbd2aec1a714cbcd5fa5f5c16
Dilmurod <nookh@ya.ru>
2024-08-12 06:06:52 +0500

    bottom add

--- src/pages/Earn.tsx
+++ src/pages/Earn.tsx
@@ -1,10 +1,14 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import BottomNav from "./BottomNav.tsx";
 const Earn = () => {
     return (
+        <>
         <div className="w-full h-full flex flex-col justify-between py-4">
             <h1>earn</h1>
         </div>
+        <BottomNav/>
+        </>
     );
 };
 export default Earn;
--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -1,10 +1,14 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import BottomNav from "./BottomNav.tsx";
 const Friends = () => {
     return (
+        <>
         <div className="w-full h-full flex flex-col justify-between py-4">
             <h1>Friends</h1>
         </div>
+        <BottomNav/>
+        </>
     );
 };
 export default Friends;
