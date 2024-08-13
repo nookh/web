cc43f36e4fc955162b835eff64781b0e217a0f98
Dilmurod <nookh@ya.ru>
2024-08-14 02:02:14 +0500

    botton

--- src/pages/BottomNav.tsx
+++ src/pages/BottomNav.tsx
@@ -19,7 +19,7 @@ const BottomNav = () => {
 	};
 	return (
         <>
-		<div className="fixed bottom-0 w-full my-0 mx-auto rounded-2xl bg-[#012237] flex items-center justify-between p-2">
+		{/* <div className="fixed bottom-0 w-full my-0 mx-auto rounded-2xl bg-[#012237] flex items-center justify-between p-2">
 			{navItems.map((item) => (
 				<div
 					key={item.title}
@@ -46,7 +46,30 @@ const BottomNav = () => {
 				</div>
 			))}
 		</div>
-        <Outlet/>
+        <Outlet/> */}
+		{/* Bottom fixed div */}
+		<div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
+        <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
+          
+          <p className="mt-1">Exchange</p>
+        </div>
+        <div className="text-center text-[#85827d] w-1/5">
+          
+          <p className="mt-1">Mine</p>
+        </div>
+        <div className="text-center text-[#85827d] w-1/5">
+          
+          <p className="mt-1">Friends</p>
+        </div>
+        <div className="text-center text-[#85827d] w-1/5">
+          
+          <p className="mt-1">Earn</p>
+        </div>
+        <div className="text-center text-[#85827d] w-1/5">
+          
+          <p className="mt-1">Airdrop</p>
+        </div>
+      </div>
         </>
 	);
 };
